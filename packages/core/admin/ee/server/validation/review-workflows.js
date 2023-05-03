d84c371f40ac1113bd46a53720c8ecf17c49ed74
nathan-pichon <nathan.pichon@strapi.io>
2023-05-03 21:12:40 +0200

    feat(review-workflows): adding content-api routes

--- packages/core/admin/ee/server/validation/review-workflows.js
+++ packages/core/admin/ee/server/validation/review-workflows.js
@@ -5,6 +5,9 @@ const { yup, validateYupSchema } = require('@strapi/utils');
 const stageObject = yup.object().shape({
   id: yup.number().integer().min(1),
   name: yup.string().max(255).required(),
+  color: yup
+    .string()
+    .matches(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i, 'Color needs to be an hexadecimal string.'),
 });
 
 const validateUpdateStagesSchema = yup.array().of(stageObject).required();
@@ -16,6 +19,10 @@ const validateUpdateStageOnEntity = yup
   .required();
 
 module.exports = {
+  validateStage: validateYupSchema(stageObject, {
+    strict: false,
+    stripUnknown: true,
+  }),
   validateUpdateStages: validateYupSchema(validateUpdateStagesSchema, {
     strict: false,
     stripUnknown: true,
